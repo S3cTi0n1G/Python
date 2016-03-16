4f6514b0e3c1a6f1488f21452becdbe927cb1518
Justin Lee <justinleeig@gmail.com>
2016-03-16 16:25:53 +0800

    Create ex13.py

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+a, b, c, d = argv
+e = raw_input("input one variale")
+print "The script is called:", a
+print "Your first variable is:", b
+print "your second variable is:", c
+print "Your third variable is:", d
+print "Your input variable is %s, e
+
+#python test.py you are you
