2d634c2d046afc0cf32979d4138e27c672803f35
Justin Lee <justinleeig@gmail.com>
2016-03-17 09:52:16 +0800

    Update ex14.py

--- ex14.py
+++ ex14.py
@@ -1,15 +1,15 @@
-from sys impory argv
+from sys import argv
 script, name = argv
 prompt = "> "
 
 print "Hi %s, I am %s,I'd ask you some qustion" % (script, name)
-print "What kind of color do you like,%s" % name
+print "What kind of color do you like?%s" % name
 color = raw_input(prompt) 
 
-print "What pet do you want,%s" % name
+print "What pet do you want?%s" % name
 pet = raw_input(prompt)
 
-Print """
+print """
 So %s, You have finish my qustion.
 Your like %s. me too
 Your want %s. good
