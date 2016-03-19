33628254552cbc8e0a15183715c91328493daf92
Justin Lee <justinleeig@gmail.com>
2016-03-19 15:02:43 +0800

    Update ex17.py

--- ex17.py
+++ ex17.py
@@ -1,1 +1,15 @@
+from sys import argv
+from os.path import exists
+script, from_file, to_file = argv
+in_file = open(from_file)
+indate = in_file.read()
+
+out_file = open(to_file)
+out_file.write(indate)
+
+out_file = open(to_file)
+print out_file.read()
+
+out_file.close
+in_file.close
 
