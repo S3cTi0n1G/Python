e2735baca338e3704b337fdd9d3886baf1b05400
Justin Lee <justinleeig@gmail.com>
2016-03-21 17:55:20 +0800

    Update ex20.py

--- ex20.py
+++ ex20.py
@@ -13,12 +13,12 @@ def print_line(line_number, f):
 current_file = open (filename)
 
 print "Whole line"
-print_all(current_file)
+print_all(current_file) #调用print_all 函数
 
-rewind(current_file)
+rewind(current_file) #调用rewind 函数
 
 current_line = 1
-print_line(current_line,current_file)
+print_line(current_line,current_file) #调用print_line 函数
 current_line += 1
 print_line(current_line, current_file)
 current_line += 1
